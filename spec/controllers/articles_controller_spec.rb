c29e8bd8007c65a374e0a6ea744c170fb3cfdfb5
Charlie Hawker <charlie.hawker@gmail.com>
2017-12-14 19:48:33 +0000

    DRY up #data_check specs with shared examples

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, vcr: true do
-
   describe 'GET show' do
     context 'variable assignment' do
       context 'when successful' do
@@ -34,56 +33,26 @@ RSpec.describe ArticlesController, vcr: true do
   end
 
   describe '#data_check' do
-    context 'an available data format is requested' do
-      methods = [
+    let(:data_check_methods) do
+      [
         {
           route: 'show',
           parameters: { article_id: 'ccdwcKYM' },
           data_url: "#{ENV['PARLIAMENT_BASE_URL']}/webarticle_by_id?webarticle_id=ccdwcKYM"
         }
       ]
-
-      before(:each) do
-        headers = { 'Accept' => 'application/rdf+xml' }
-        request.headers.merge(headers)
-      end
-
-      it 'should have a response with http status redirect (302)' do
-        methods.each do |method|
-          if method.include?(:parameters)
-            get method[:route].to_sym, params: method[:parameters]
-          else
-            get method[:route].to_sym
-          end
-          expect(response).to have_http_status(302)
-        end
-      end
-
-      it 'redirects to the data service' do
-        methods.each do |method|
-          if method.include?(:parameters)
-            get method[:route].to_sym, params: method[:parameters]
-          else
-            get method[:route].to_sym
-          end
-          expect(response).to redirect_to(method[:data_url])
-        end
-      end
-
     end
+
+    it_behaves_like 'a data service request'
 
     context 'an unavailable data format is requested' do
       before(:each) do
         headers = { 'Accept' => 'application/foo' }
         request.headers.merge(headers)
       end
-
       it 'should raise ActionController::UnknownFormat error' do
         expect{ get :show, params: { article_id: 'ccdwcKYM' } }.to raise_error(ActionController::UnknownFormat)
       end
     end
   end
-
-
-
 end

87ee72d84aa25c6513c0e1340753ffe066e32b96
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-12-15 10:13:56 +0000

    Add articles#show controller, view and tests

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  before_action :data_check, :build_request
+
+  ROUTE_MAP = {
+    show: proc { |params| Parliament::Utils::Helpers::ParliamentHelper.parliament_request.webarticle_by_id.set_url_params({ webarticle_id: params[:article_id] }) },
+  }.freeze
+
+  def show
+    articles = Parliament::Utils::Helpers::RequestHelper.filter_response_data(
+      @request,
+      Parliament::Utils::Helpers::RequestHelper.namespace_uri_schema_path('Article')
+    )
+
+    @article = articles.find { |a| a.graph_id == params[:article_id] }
+
+    raise ActionController::RoutingError, 'Article Not Found' unless @article
+  end
+
+end

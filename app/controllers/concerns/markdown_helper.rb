f62eeafcf97308dbf87c5fb23e5f58d6c7a85ac0
joshuawaheed <joshua.waheed@gmail.com>
2017-12-18 11:27:30 +0000

    GTP: Template cleanup. Allow tables in markdown helper.

--- app/controllers/concerns/markdown_helper.rb
+++ app/controllers/concerns/markdown_helper.rb
@@ -1,7 +1,7 @@
 module MarkdownHelper
 
   def self.markdown(template)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
     return markdown.render(template).html_safe
   end
 

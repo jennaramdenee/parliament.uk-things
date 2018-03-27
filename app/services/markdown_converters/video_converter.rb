42e8a13a20344f3a49af6c11fbcddb45396738df
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-03-27 17:45:29 +0100

    Make start and end time for a video optional

--- app/services/markdown_converters/video_converter.rb
+++ app/services/markdown_converters/video_converter.rb
@@ -8,8 +8,8 @@ module MarkdownConverter
         video_links_array.each do |video_link|
           original_video_link = video_link[0]
           id = video_link[1]
-          start_time = video_link[2]
-          end_time = video_link[3]
+          start_time = video_link[3]
+          end_time = video_link[4]
 
           converted_video_html = create_video_html(id, start_time, end_time)
           markdown.gsub!(original_video_link, converted_video_html)
@@ -20,11 +20,17 @@ module MarkdownConverter
       private
 
       def extract_video_links(markdown)
-        markdown.scan(/(\[video id:(\S{36}) start:(\S+) end:(\S+)\])/)
+        # regex = Regexp.union(/(\[video id:(\S{36})\])/, /start:(\S+) end:(\S+)/)
+        # markdown.scan(regex)
+        # markdown.scan(/(\[video id:(\S{36}) start:(\S+) end:(\S+)\])/)
+        markdown.scan(/(\[video id:(\S{36})( start:(\S+) end:(\S+))?\])/)
+
       end
 
-      def create_video_html(id, start_time, end_time)
-        "<iframe src='https://videoplayback.parliamentlive.tv/Player/Index/#{id}?in=1900-01-01T#{encode_video_times(start_time)}%2B01%3A00&amp;out=1900-01-01T#{encode_video_times(end_time)}2B01%3A00&amp;audioOnly=False&amp;autoStart=False&amp;statsEnabled=False' id='UKPPlayer' name='UKPPlayer' title='UK Parliament Player' seamless='seamless' frameborder='0' allowfullscreen style='width:100%;height:100%;'></iframe>"
+      def create_video_html(id, start_time = nil, end_time = nil)
+        video_html_string = "<iframe src='https://videoplayback.parliamentlive.tv/Player/Index/#{id}?"
+        video_html_string += "in=1900-01-01T#{encode_video_times(start_time)}%2B01%3A00&amp;out=1900-01-01T#{encode_video_times(end_time)}2B01%3A00&amp;" if start_time && end_time
+        video_html_string += "audioOnly=False&amp;autoStart=False&amp;statsEnabled=False' id='UKPPlayer' name='UKPPlayer' title='UK Parliament Player' seamless='seamless' frameborder='0' allowfullscreen style='width:100%;height:100%;'></iframe>"
       end
 
       def encode_video_times(time)
@@ -33,3 +39,6 @@ module MarkdownConverter
     end
   end
 end
+
+
+# (\[video id:(\S{36}) start:(\S+) end:(\S+)\]) | (\[video id:(\S{36})\])
--- spec/services/markdown_converters/video_converter_spec.rb
+++ spec/services/markdown_converters/video_converter_spec.rb
@@ -21,13 +21,21 @@ RSpec.describe MarkdownConverter::VideoConverter do
 
   context '#extract_video_links' do
     it 'can extract video links' do
-      expect(MarkdownConverter::VideoConverter.send(:extract_video_links, original_article.article_body)).to eq([["[video id:114c33a1-f412-44a8-b5c1-f1822b895f46 start:06:50:00 end:07:00:00]", "114c33a1-f412-44a8-b5c1-f1822b895f46", "06:50:00", "07:00:00"]])
+      expect(MarkdownConverter::VideoConverter.send(:extract_video_links, original_article.article_body)).to eq([["[video id:114c33a1-f412-44a8-b5c1-f1822b895f46 start:06:50:00 end:07:00:00]", "114c33a1-f412-44a8-b5c1-f1822b895f46", " start:06:50:00 end:07:00:00", "06:50:00", "07:00:00"], ["[video id:bf2334ce-e87f-4224-9a81-9a0e9886f19b]", "bf2334ce-e87f-4224-9a81-9a0e9886f19b", nil, nil, nil]])
     end
   end
 
   context '#create_video_html' do
-    it 'can convert a video link to HTML' do
-      expect(MarkdownConverter::VideoConverter.send(:create_video_html, '114c33a1-f412-44a8-b5c1-f1822b895f46', '15:00:00', '16:30:00')).to eq("<iframe src='https://videoplayback.parliamentlive.tv/Player/Index/114c33a1-f412-44a8-b5c1-f1822b895f46?in=1900-01-01T15%3A00%3A00%2B01%3A00&amp;out=1900-01-01T16%3A30%3A002B01%3A00&amp;audioOnly=False&amp;autoStart=False&amp;statsEnabled=False' id='UKPPlayer' name='UKPPlayer' title='UK Parliament Player' seamless='seamless' frameborder='0' allowfullscreen style='width:100%;height:100%;'></iframe>")
+    context 'with start and end time' do
+      it 'can convert a video link to HTML' do
+        expect(MarkdownConverter::VideoConverter.send(:create_video_html, '114c33a1-f412-44a8-b5c1-f1822b895f46', '15:00:00', '16:30:00')).to eq("<iframe src='https://videoplayback.parliamentlive.tv/Player/Index/114c33a1-f412-44a8-b5c1-f1822b895f46?in=1900-01-01T15%3A00%3A00%2B01%3A00&amp;out=1900-01-01T16%3A30%3A002B01%3A00&amp;audioOnly=False&amp;autoStart=False&amp;statsEnabled=False' id='UKPPlayer' name='UKPPlayer' title='UK Parliament Player' seamless='seamless' frameborder='0' allowfullscreen style='width:100%;height:100%;'></iframe>")
+      end
+    end
+
+    context 'with no start or end time' do
+      it 'can convert a video link to HMTL' do
+        expect(MarkdownConverter::VideoConverter.send(:create_video_html, '114c33a1-f412-44a8-b5c1-f1822b895f46')).to eq("<iframe src='https://videoplayback.parliamentlive.tv/Player/Index/114c33a1-f412-44a8-b5c1-f1822b895f46?audioOnly=False&amp;autoStart=False&amp;statsEnabled=False' id='UKPPlayer' name='UKPPlayer' title='UK Parliament Player' seamless='seamless' frameborder='0' allowfullscreen style='width:100%;height:100%;'></iframe>")
+      end
     end
   end
 

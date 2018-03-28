d1604c7956e86b10c2f57c83422a67d41c9e22b6
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-03-28 13:09:25 +0100

    Update pugin

--- spec/views/collections/delimited_links.html.haml_spec.rb
+++ spec/views/collections/delimited_links.html.haml_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe 'collections/_delimited_links' do
   context 'collection' do
     context 'converted to HTML' do
       it 'name will render correctly' do
-        expect(rendered).to match(/<p><a href="\/collections\/xxxxxxx1">This is test collection 1.<\/a><\/p>/)
+        expect(rendered).to match(/<a href="\/collections\/xxxxxxx1">This is test collection 1.<\/a>/)
       end
     end
 
@@ -34,10 +34,15 @@ RSpec.describe 'collections/_delimited_links' do
       let!(:collection_name_text1) { '<script>This is test collection 1.</script>' }
 
       it 'sanitized name will render correctly' do
-        expect(rendered).to match(/<p><a href="\/collections\/xxxxxxx1">This is test collection 1.<\/a><\/p>/)
+        expect(rendered).to match(/<a href="\/collections\/xxxxxxx1">This is test collection 1.<\/a>/)
       end
     end
+  end
 
+  context 'listing collections' do
+    it 'renders comma separated links to each collection' do
+      expect(rendered).to include("<p>\n<a href=\"/collections/xxxxxxx1\">This is test collection 1.</a>\n, \n<a href=\"/collections/xxxxxxx2\">This is test collection 2.</a>\n\n</p>\n")
+    end
   end
 
 end

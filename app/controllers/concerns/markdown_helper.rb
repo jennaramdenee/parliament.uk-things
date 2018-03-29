94265c9883f0543db426c907e8aacd8a807ed751
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-03-29 18:01:09 +0100

    Use ParliamentMarkdownRenderer as renderer

--- app/controllers/concerns/markdown_helper.rb
+++ app/controllers/concerns/markdown_helper.rb
@@ -1,12 +1,11 @@
 require_relative '../../services/markdown_converter'
 
 module MarkdownHelper
-
   # Uses redcarpet gem to convert markdown into HTML, with chosen HTML extensions
   #
   # @return template [String] Template as HTML
   def self.markdown(template)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
+    markdown = Redcarpet::Markdown.new(ParliamentMarkdownRenderer, tables: true, autolink: true)
     html = markdown.render(ActionController::Base.helpers.sanitize(template))
     ActionController::Base.helpers.sanitize(html).html_safe
   end

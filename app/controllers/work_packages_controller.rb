0a5415d2e4d3512f88a0d179f78fb92ac16e2f17
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-05-23 18:40:11 +0100

    >

--- app/controllers/work_packages_controller.rb
+++ app/controllers/work_packages_controller.rb
@@ -22,10 +22,26 @@ class WorkPackagesController < ApplicationController
     @procedure = @procedure.first
 
 
+    @ordered_procedure_steps = []
     # Order business items by where they appear in the procedure
     # @ordered_business_items = @actualized_procedure_steps.sort_by(:business_item_date, :distance_from_origin, :name).map(&:business_item).uniq
+    # @ordered_business_items = @actualized_procedure_steps.sort_by(:distance_from_origin, :name).map(&:business_item).uniq
     # @ordered_business_items = @actualized_procedure_steps.sort_by(:distance_from_origin, :business_item_date, :name).map(&:business_item).uniq
-    @ordered_business_items = @actualized_procedure_steps.sort_by(:distance_from_origin, :name).map(&:business_item).uniq
+
+    @x = @actualized_procedure_steps.group_by { |step| step.distance_from_origin }
+
+    # Go through each bucket
+    @x.each do |distance, steps|
+      # Sort array of steps by business item date, put nil at the end
+      steps = steps.sort_by(&:business_item_date)
+      # Add to an array of steps
+      @ordered_procedure_steps << steps
+    end
+
+    # goghwoiwrghoih
+
+    # Map to business item and remove duplicates
+    @ordered_business_items = @ordered_procedure_steps.flatten!.map(&:business_item)
 
     # Group business items by their date
     @grouped_and_ordered_business_items = BusinessItemGroupingHelper.group(@ordered_business_items, :date)
